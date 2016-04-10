7984de30543beb39b4af266f202677d79f81082a
Zhulanov Anton <zhulanoff2007@gmail.com>
2016-04-11 00:21:52 +0500

    Adapt to active record

--- app.rb
+++ app.rb
@@ -1,41 +1,10 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
-# require 'sqlite3'
+require 'sinatra/activerecord'
 
-#examle access to db
-# def get_db 
-#   db = SQLite3::Database.new './database/leprosorium.sqlite'
-#   db.results_as_hash = true
-#   db
-# end
-
-#example set variable before POST or GET request
-# before do 
-#   @db = get_db
-# end
-
-#example congfiure db after reload page
-# configure do 
-#   get_db.execute 'CREATE TABLE IF NOT EXISTS "posts"
-#   (
-#   "id" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL UNIQUE,
-#   "created_at" DATETIME NOT NULL,
-#   "name_post" TEXT NOT NULL,
-#   "name_author" TEXT NOT NULL,
-#   "content" TEXT NOT NULL 
-#   )'
-
-#   get_db.execute 'CREATE TABLE IF NOT EXISTS "comments"
-#   (
-#   "id" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL UNIQUE,
-#   "created_at" DATETIME NOT NULL,
-#   "content" TEXT NOT NULL ,
-#   "name_author" TEXT NOT NULL ,
-#   "fk_post" INTEGER NOT NULL
-#   )'
-# end
+set :database, "sqlite3:./database/barbershop.db"
 
 get '/' do
-	erb 'Hello, i`m clean project'
+	erb 'Hello!'
 end
